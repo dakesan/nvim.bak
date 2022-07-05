0a0eee5774a0c542bb3982940df7c9cf19e1d13c
dakesan <dakesan@gmail.com>
2022-07-06 07:24:57 +0900

    fixed color scheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,20 @@
-vim.api.nvim_set_option('termguicolors', true)
--- utils.opt('o', 'termguicolors', true)
+if vim.fn.exists('g:vscode') == 0 then
+    vim.api.nvim_set_option('termguicolors', true)
+    vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+    vim.cmd [[colorscheme catppuccin]]
+    vim.cmd 'syntax enable'
+end
 
+if vim.fn.exists('g:vscode') ~= 0 then
+    vim.cmd 'set t_Co=0'
+    vim.cmd 'syntax off'
+    vim.cmd 'filetype off'
+end
 
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-vim.cmd[[colorscheme catppuccin]]
+---Disables built in matchparen plugin
+local function disable_builtin()
+    vim.g.loaded_matchparen = 1
+    if fn.exists(':NoMatchParen') ~= 0 then
+        vim.api.nvim_command('NoMatchParen')
+    end
+end
