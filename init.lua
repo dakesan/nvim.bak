f133ec335b381d1d65a65b6642ac61732952d9b6
dakesan <dakesan@gmail.com>
2022-08-10 08:07:45 +0900

    clip.exe works!!!

--- init.lua
+++ init.lua
@@ -27,3 +27,12 @@ vim.cmd[[
         \   'cache_enabled': 0,
         \ }
 ]]
+
+vim.cmd[[
+    if system('uname -a | grep microsoft') != ''
+  augroup myYank
+    autocmd!
+    autocmd TextYankPost * :call system('clip.exe', @")
+  augroup END
+endif"
+]]
